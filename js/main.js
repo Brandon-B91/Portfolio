17c122dbcfc054ad05cdb8ac2918c2ad247271d5
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2019-08-25 21:25:18 -0600

    updates
    Updates to performance.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 
 const modal = document.getElementById('modal');
 const modalImg = document.querySelector('.modalImg');
-let imgs = document.querySelectorAll('.img');
-let closeMdl = document.getElementById('close');
+const imgs = document.querySelectorAll('.img');
+const closeMdl = document.getElementById('close');
 
 imgs.forEach((img) => {
   img.addEventListener('click', function mdl() {
     modal.style.display = 'block';
     modalImg.src = this.src;
-  })
-})
+  });
+});
 
 modal.addEventListener('click', (e) => {
   if(e.target == modal){

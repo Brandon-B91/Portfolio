e034e6b34c4b8dd9710bb95eda6611831d721fde
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2020-08-17 14:22:09 -0600

    css updates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-
 const modal = document.getElementById('modal');
 const modalImg = document.querySelector('.modalImg');
 const imgs = document.querySelectorAll('.img');
 const closeMdl = document.getElementById('close');
 
 imgs.forEach((img) => {
-  img.addEventListener('click', function mdl() {
-    modal.style.display = 'block';
-    modalImg.src = this.src;
-  });
+    img.addEventListener('click', function mdl() {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+    });
 });
 
 modal.addEventListener('click', (e) => {
-  if(e.target == modal){
-    modal.style.display = 'none'
-  }
+    if (e.target == modal) {
+        modal.style.display = 'none'
+    }
 })
 
 closeMdl.addEventListener('click', () => {
-  modal.style.display = 'none'
+    modal.style.display = 'none'
 })
